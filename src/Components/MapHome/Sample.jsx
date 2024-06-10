2815ca9ee0296f3a791207e66244a431d45aa97f
Kokoro-Salmon <akuvarna19@gmail.com>
2024-06-10 18:07:01 +0530

    minor changes

--- src/Components/MapHome/Sample.jsx
+++ src/Components/MapHome/Sample.jsx
@@ -6,7 +6,7 @@ export default function Sample() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, // Add your API key
   });
-  console.log("Bhay Api of Map Is Working");
+  console.log("Brother Api of Map Is Working");
 
   return isLoaded ? <Map /> : <div>Loading...</div>;
 }
